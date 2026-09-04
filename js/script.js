81fa6572bcd520f5bcb50b1909599638f3d92710
Sohel Rana <mdsohelrana191807@gmail.com>
2026-09-04 22:32:51 +0600

    s

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
 let x = document.getElementById('dark')
 
-dark.addEventListener('click',()=>{
+dark.addEventListener('click', () => {
     document.body.style.background = '#000'
 })
+
+
+
+
+
+function lig() {
+    document.getElementById('light').innerHTML = "Nice"
+    document.body.style.background = "#fff"
+
+}
+
+
+
+let sub = document.getElementById('sub')
+
+
+sub.addEventListener('click', () => {
+
+    let inputinp =inp.value
+
+    if (inputinp > 18) {
+        alert("adult")
+
+    }
+
+    else{
+        alert("child")
+    }
+
+})
+
