12a50e1ce552ad4d9c6ea1509be72649fa3bb96a
Sohel Rana <mdsohelrana191807@gmail.com>
2026-09-06 22:46:41 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,101 +1,36 @@
-let x = document.getElementById('dark')
+const bgColor =document.getElementById('bgColor');
+const colorBtn =document.getElementById('colorBtn');
 
-dark.addEventListener('click', () => {
-    document.body.style.background = '#2c1b1b'
+colorBtn.addEventListener('click',()=>{
+
+    const backcolor = randomColor();
+     bgColor.style.background = backcolor;
+
 })
 
 
+function randomColor(){
+    const red =Math.floor(Math.random()*255);
+    const green =Math.floor(Math.random()*255);
+    const blue =Math.floor(Math.random()*255);
 
-
-
-function lig() {
-    document.getElementById('light').innerHTML = "Nice"
-    document.body.style.background = "#cfd8e6"
-
+    return `rgb(${red},${green},${blue})`
 }
 
 
 
-let sub = document.getElementById('sub')
+let moon =document.getElementById('moon')
 
-
-sub.addEventListener('click', () => {
-
-    let inputinp = inp.value
-
-    if (inputinp > 18) {
-        alert("adult")
-
-    }
-
-    else {
-        alert("child")
-    }
+moon.addEventListener('click', ()=>{
+    bgColor.style.background ="#000"
+    headtext.style.color="#fff"
 
 })
 
 
-function sls() {
 
-    let input = document.getElementById("sks")
-    if (input.value === "") {
-        alert("fill the box")
-
-    }
-    else {
-        alert(" your name is" + input.value)
-    }
-
-}
-
-// function dks() {
-
-//     let inputbtn = document.getElementById('inputbtn');
-//     let deleteMe = document.getElementById('deleteMe');
-//     // let deleted = document.getElementById('deleted')
-
-
-
-//     if (inputbtn.value === "Delete") {
-//         deletes.remove();
-
-//     }
-//     else {
-//         alert('please type Delete')
-//     }
-
-
-// }
-
-
-
-
-let deletes = document.getElementById("deletes")
-let inputbtn = document.getElementById("inputbtn")
-let deleted = document.getElementById("deleted")
-
-
-deleted.addEventListener("click", () => {
-    if (inputbtn.value === "delete") {
-        deletes.remove();
-        inputbtn.remove=""
-
-    }
-    else {
-        alert('delete nam e lekho')
-    }
+let sun =document.getElementById('sun')
+sun.addEventListener('click',()=>{
+    bgColor.style.background="#fff"
+     headtext.style.color="#000"
 })
-
-
-let red = document.getElementById("red")
-
-
-red.addEventListener('click', ()=>{
-    document.body.style.background = "red"
-    document.getElementById("red").innerHTML = 'blue'
-})
-
-function grn (){
-    let green = document.getElementById("green")
-    document.body.style.background = 'green'
-}
